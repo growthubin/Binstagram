483b3888d3067da0c545f0c974a739d7a443cff6
Subin Park <sbinp5818@khu.ac.kr>
2022-11-27 15:15:25 +0900

    좋아요/댓글/북마크 기능 추가(1/2)

--- content/models.py
+++ content/models.py
@@ -5,9 +5,25 @@ from django.db import models
 class Feed(models.Model):
     content = models.TextField()  # 글 내용
     image = models.TextField()  # 피드 이미지
-    profile_image = models.TextField()  # 프로필 이미지
-    user_id = models.TextField()  # 글쓴이
+    email = models.EmailField(default="")  # 글쓴이
     like_count = models.IntegerField()  # 좋아요 수
 
     class Meta:
         db_table = "content_feed"
+
+
+class Like(models.Model):
+    feed_id = models.IntegerField(default=0)
+    email = models.EmailField(default='')  # 좋아요 누른 사람
+    is_like = models.BooleanField(default=True)
+    # 1 try1@gmail.com Y
+
+class Reply(models.Model):
+    feed_id = models.IntegerField(default=0)
+    email = models.EmailField(default='')
+    reply_content = models.TextField()
+
+class BookMark(models.Model):
+    feed_id = models.IntegerField(default=0)
+    email = models.EmailField(default='')  # 좋아요 누른 사람
+    is_marked = models.BooleanField(default=True)
--- content/views.py
+++ content/views.py
@@ -2,7 +2,7 @@ from uuid import uuid4
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Feed  # Feed라는 클래스를 가져올 거다
+from .models import Feed, Reply  # Feed라는 클래스를 가져올 거다
 from user.models import User
 import os
 from Binstagram.settings import MEDIA_ROOT
@@ -10,7 +10,29 @@ from Binstagram.settings import MEDIA_ROOT
 
 class Main(APIView):  # Sub 클래스
     def get(self, request):  # get으로 호출하면 get 함수 실행
-        feed_list = Feed.objects.all().order_by('-id')  # select * from content_feed;
+        feed_object_list = Feed.objects.all().order_by('-id')  # select * from content_feed;
+        feed_list = []
+
+        # Feed.objects 안에 사용자 정보가 email밖에 없으므로 feed_list에 피드에 표시할 정보를 하나씩 append
+        for feed in feed_object_list:
+            user = User.objects.filter(email=feed.email).first()  # 유저 객체(email)를 찾고
+            reply_object_list = Reply.objects.filter(feed_id=feed.id)  # 해당 feed id에 해당하는 댓글 리스트 찾기
+            reply_list = []
+
+            # 댓글 목록
+            for reply in reply_object_list:
+                user = User.objects.filter(email=reply.email).first()
+                reply_list.append(dict(reply_content=reply.reply_content,
+                                       nickname=user.nickname))
+
+            # 피드 이미지랑 내용, 좋아요 개수, 댓글 리스트 등 표시
+            feed_list.append(dict(image=feed.image,
+                                  content=feed.content,
+                                  like_count=feed.like_count,
+                                  profile_image=user.profile_image,
+                                  nickname=user.nickname,
+                                  reply_list=reply_list
+                                  ))
 
         email = request.session.get('email', None)
         if email is None:
@@ -38,11 +60,10 @@ class UploadFeed(APIView):
 
         image = uuid_name
         content = request.data.get('content')
-        user_id = request.data.get('user_id')
-        profile_image = request.data.get('profile_image')
+        email = request.session.get('email', None)
 
         # 피드에 저장(피드에 업로드한 정보 프론트에 추가하기)
-        Feed.objects.create(image=image, content=content, user_id=user_id, profile_image=profile_image, like_count=0)
+        Feed.objects.create(image=image, content=content, email=email, like_count=0)
 
         return Response(status=200)
 
@@ -58,3 +79,14 @@ class Profile(APIView):
             return render(request, "user/login.html")  # 사용자 정보가 없어도 로그인 페이지만 보여줌(다시 로그인 시도해라)
 
         return render(request, 'content/profile.html', context=dict(user=user))
+
+
+class UploadReply(APIView):
+    def post(self, request):
+        feed_id = request.data.get('feed_id', None)
+        reply_content = request.data.get('reply_content', None)
+        email = request.session.get('email', None)
+
+        Reply.objects.create(feed_id=feed_id, reply_content=reply_content, email=email)
+
+        return Response(status=200)
